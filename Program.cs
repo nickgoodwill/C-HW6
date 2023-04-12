cdfdf6d753b0f409c85d6f42dad8050c29666d8d
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-13 00:33:45 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -33,3 +33,35 @@ int PositivSum(int[] numbers)
 
 	return result;
 }
+
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+int Read (string arg)
+{
+System.Console.WriteLine($"Введите число: {arg} ");
+return int.Parse(System.Console.ReadLine());
+}
+
+int K1 = Read("K1");
+int K2 = Read("K2");
+int B1 = Read("B1");
+int B2 = Read("B2");
+
+int x = 0;
+int y = 0;
+
+if (K1 == K2)
+{
+    Console.WriteLine("Линии параллельны");
+}
+else
+{
+    x = (B2 - B1) / (K1 - K2);
+    y = K1 * x + B1;
+    Console.WriteLine($"Точка пересичения линий: x = {x} ; y = {y}");
+}
