577dcb76610e9236dcb29784349db6a5d256c610
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-12 22:38:12 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через пробел и нажмите enter");
+int[] numbers = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+
+PrintArray(numbers);
+Console.WriteLine(PositivSum(numbers));
+
+
+void PrintArray(int[] numbers)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < numbers.Length-1; i++)
+    {
+        System.Console.Write(numbers[i] + "; ");
+    }
+    System.Console.WriteLine(numbers[numbers.Length -1] + "]");
+}
+
+int PositivSum(int[] numbers)
+{
+	int result = 0;
+
+	for (int i = 0; i < numbers.Length; i++)
+	{
+		if (numbers[i] > 0)
+        {
+            result+=1;
+        }
+	}
+
+	return result;
+}
